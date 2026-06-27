48fda20898eee69299c7e27bd04202dc4dbcf7b5
mohamed hilli <75916919+mohamedhill@users.noreply.github.com>
2026-06-27 01:13:46 +0100

    Time: 0 ms (100%), Space: 2.2 MB (31.82%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn first_bad_version(&self, n: i32) -> i32 {
+        let mut left = 1;
+        let mut right = n;
+
+        while left < right {
+            let mid = left + (right - left) / 2;
+
+            if self.isBadVersion(mid) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        left
+    }
+}
